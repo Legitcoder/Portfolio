9ccb44ba135fb70064b7c501fc11e415d105fb45
Moin Uddin <U_Moin75@yahoo.com>
2018-08-04 23:50:35 -0400

    Add media queries adding responsive design and scrolling navigation ability

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
 // let experimentGradient = "linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%)"
 
 
-
 document.addEventListener("DOMContentLoaded", (e) => {
     let concreteJungle = {"mp4": "img/Concrete_Jungle/Concrete_Jungle.mp4", "ogv": "img/Concrete_Jungle/Concrete_Jungle.ogv", "webm": "img/Concrete_Jungle/Concrete_Jungle.webm" };
     let loveCoding = {"mp4": "img/Love-Coding/Love-Coding.mp4", "ogv": "img/Love-Coding/Love-Coding.ogv", "webm": "img/Love-Coding/Love-Coding.webm" };
@@ -31,6 +30,104 @@ document.addEventListener("DOMContentLoaded", (e) => {
         coverVideo.load();
     }
        setInterval(coverShowCase, 10000);
+
+       let copyRight = document.querySelector('.footer > p');
+       copyRight.innerText = `Copyright © ${(new Date).getFullYear()} Moin Uddin`
+
+
+
+
+       //Navigation Scroll Animation
+
+       let projectsLink = document.querySelector('.navigation__link--projects');
+       let contactLink = document.querySelector('.navigation__link--contact');
+
+       function scrollIt(destination, duration = 200, easing = 'linear', callback) {
+
+        const easings = {
+          linear(t) {
+            return t;
+          },
+          easeInQuad(t) {
+            return t * t;
+          },
+          easeOutQuad(t) {
+            return t * (2 - t);
+          },
+          easeInOutQuad(t) {
+            return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+          },
+          easeInCubic(t) {
+            return t * t * t;
+          },
+          easeOutCubic(t) {
+            return (--t) * t * t + 1;
+          },
+          easeInOutCubic(t) {
+            return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+          },
+          easeInQuart(t) {
+            return t * t * t * t;
+          },
+          easeOutQuart(t) {
+            return 1 - (--t) * t * t * t;
+          },
+          easeInOutQuart(t) {
+            return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
+          },
+          easeInQuint(t) {
+            return t * t * t * t * t;
+          },
+          easeOutQuint(t) {
+            return 1 + (--t) * t * t * t * t;
+          },
+          easeInOutQuint(t) {
+            return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
+          }
+        };
+      
+        const start = window.pageYOffset;
+        const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+      
+        const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+        const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+        const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset) - 90;
+      
+        if ('requestAnimationFrame' in window === false) {
+          window.scroll(0, destinationOffsetToScroll);
+          if (callback) {
+            callback();
+          }
+          return;
+        }
+      
+        function scroll() {
+          const now = 'now' in window.performance ? performance.now() : new Date().getTime();
+          const time = Math.min(1, ((now - startTime) / duration));
+          const timeFunction = easings[easing](time);
+          window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+      
+          if (window.pageYOffset === destinationOffsetToScroll) {
+            if (callback) {
+              callback();
+            }
+            return;
+          }
+      
+          requestAnimationFrame(scroll);
+        }
+      
+        scroll();
+      }
+
+      projectsLink.addEventListener('click', () => scrollIt(document.querySelector('#projects'), 500));
+
+      contactLink.addEventListener('click', () => scrollIt(document.querySelector('#contact'), 500));
+
+
+
+
   });
 
 window.addEventListener('scroll', (e)=> {
